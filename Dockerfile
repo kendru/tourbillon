6561dcb95f5b343d4d7fd661a3eefc1f1b414bec
Andrew Meredith <andrew@lambdasoftware.com>
2017-07-14 15:02:34 -0600

    Fix envvar name; Fix schema syntax error

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM clojure:lein-alpine
 
 EXPOSE 80
-ENV WEB_PORT="80" APP_ENV="production"
+ENV HTTP_PORT="80" APP_ENV="prod"
 
 RUN mkdir -p /srv
 ADD target/tourbillon-standalone.jar /srv/tourbillon.jar
--- scripts/sql/postgres_schema.sql
+++ scripts/sql/postgres_schema.sql
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS events (
     job_id VARCHAR,
     "start" BIGINT NOT NULL,
     "interval" TEXT NULL,
-    data BYTEA
+    data BYTEA,
     is_expired BOOLEAN NOT NULL DEFAULT false,
     PRIMARY KEY (id, job_id, "start")
 );
